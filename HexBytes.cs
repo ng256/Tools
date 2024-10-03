6342ed85a72092d2a18aa9480e3a6a0e62865d07
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-10-03 13:38:00 +0300

    Add files via upload

---
+++ HexBytes.cs
@@ -0,0 +1,17 @@
+public class Data
+{
+    [XmlIgnore()]
+    public uint Value { get; set; }
+    [XmlAttribute("Value", DataType = "hexBinary")]
+    public byte[] ValueBinary
+    {
+        get
+        {
+            return BitConverter.GetBytes(Value).Reverse().ToArray();
+        }
+        set
+        {
+            Value = BitConverter.ToUInt32(value.Reverse().ToArray(), 0);
+        }
+    }
+}
