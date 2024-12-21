82e90f7e64f631367c63b6cacbba010321b9b219
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-12-21 23:44:39 +0300

    Update Disposable.cs

--- Disposable.cs
+++ Disposable.cs
@@ -1,30 +1,40 @@
-/*
- *  Disposable.cs
- *
- *  A base class that provides a robust implementation of the IDisposable pattern.
- *  Automatically disposes properties and fields of the instance that implement
- *  IDisposable, ensuring resources are released correctly.
- *
- *  Copyright (c) 2024 Bashkardin Pavel
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
+/***************************************************************
+
+    File: Disposable.cs
+
+    Description:
+
+    A base class that provides  a  robust implementation  of the
+    disposing  pattern.  Automatically  disposes  properties and
+    fields  of the instance  that implement IDisposable and / or
+    IAsyncDisposable ensuring resources are released correctly.
+
+    Copyright: (c) 2024 Bashkardin Pavel
+
+    Permission  is hereby granted, free of charge, to any person
+    obtaining  a  copy    of    this  software    and associated
+    documentation   files  (the "Software"),    to  deal  in the
+    Software without  restriction, including  without limitation
+    the rights to use, copy, modify, merge, publish, distribute,
+    sublicense,  and/or  sell  copies   of the Software,  and to
+    permit persons to whom the Software  is furnished to  do so,
+    subject to  the  following  conditions:
+
+    The above copyright notice  and this permission notice shall
+    be  included  in  all copies  or substantial portions of the
+    Software.
+
+    THE SOFTWARE IS  PROVIDED  "AS IS", WITHOUT WARRANTY  OF ANY
+    KIND,  EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    WARRANTIES OF MERCHANTABILITY,  FITNESS    FOR A  PARTICULAR
+    PURPOSE  AND NONINFRINGEMENT. IN NO EVENT SHALL  THE AUTHORS
+    OR COPYRIGHT HOLDERS  BE   LIABLE FOR ANY CLAIM,  DAMAGES OR
+    OTHER  LIABILITY,  WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    OTHERWISE, ARISING FROM, OUT OF   OR IN CONNECTION  WITH THE
+    SOFTWARE  OR THE USE  OR  OTHER  DEALINGS IN   THE SOFTWARE.
+
+***************************************************************/
+
 
 using System.Collections;
 using System.Reflection;
@@ -33,7 +43,8 @@ using System.Reflection;
 namespace System
 {
     /// <summary>
-    /// A base class that provides a robust implementation of the IDisposable pattern.
+    /// A base class that provides a robust implementation of the <see cref="IDisposable"/>
+    /// and/or <see cref="IAsyncDisposable"/> pattern.
     /// Automatically disposes properties and fields of the instance that implement IDisposable,
     /// ensuring resources are released correctly.
     /// </summary>
